62bc945fa21f71bf5ecb7dc99b81c405ce4b4eb0
Gifty Nkrumah <aksalnkrumah@gmail.com>
2020-12-14 16:27:46 +0000

    models-seekclarification

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,7 +21,9 @@ exports.new = function (request, response) {
     let book = new Book()
     book.title = request.body.title? request.body.title: book.title
     book.author = request.body.author
-    book.genre = request.body.genre
+    book.number_of_pages = request.body.number_of_pages
+    book.category = request.body.category
+    book.rating = request.body.rating
 
     book.save(function (err) {
         if (err)
@@ -29,6 +31,32 @@ exports.new = function (request, response) {
         response.json({
             message: 'New book created',
             data: book
+        })
+    })
+}
+
+// find a book
+exports.view = function(request, response) {
+    Book.findById(request.params.book_id, function (err, book) {
+        if (err)
+            response.send(err)
+        response.json({
+            message: '1 book found',
+            data: book
+        })
+    })
+}
+
+//  delete a book
+exports.delete = function (request, response) {
+    Book.deleteOne({
+        _id: request.params.book_id
+    }, function (err, book){
+        if (err)
+            response.send(err)
+        response.json({
+            status: 'success',
+            message: 'Book deleted'
         })
     })
 }
